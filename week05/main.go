744a3efc38a890ffabf849c5385c7ee7fba457b8
202244003 <202244003a@gmail.com>
2023-09-25 14:59:26 +0900

    week05 v0.6

--- week05/main.go
+++ week05/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputNumber, _ := reader.ReadString('\n') // option 1, Ignore The error return value with blank
+	inputNumber, err := reader.ReadString('\n') // option 2
+	log.Fatal(err)
 	fmt.Println(inputNumber)
 }
