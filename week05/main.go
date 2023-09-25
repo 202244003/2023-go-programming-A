6d34e8fff43bae5caba8695ba105eb6b0c3d35b2
202244003 <202244003a@gmail.com>
2023-09-25 15:19:51 +0900

    week05 v0.9

--- week05/main.go
+++ week05/main.go
@@ -5,19 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputNumber, err := reader.ReadString('\n') // option 2
+	inputNumberString, err := reader.ReadString('\n') // option 2
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	inputNumberString = strings.TrimSpace(inputNumberString)
+	inputNumber, err := strconv.ParseFloat(inputNumberString, 32)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if inputNumber >= 90 {
 		grade := "A grade!"
 	} else {
 		grade := "BCDE grade~"
 	}
-	fmt.Println(grade)
+	fmt.Println("You got " + grade)
 }
